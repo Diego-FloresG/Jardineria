305e06ab36d65f0252161bba2b7be70e922c3443
AlejandroOliver12 <71462239+AlejandroOliver12@users.noreply.github.com>
2021-03-10 09:43:12 -0800

    Update datos.sql

--- datos.sql
+++ datos.sql
@@ -5,7 +5,15 @@ insert into Empresas values
 
 /******************167******************/
 insert into Empresas values
-
+(167,'Jardineria2000s.a','Jardineria2000s.a',' Av.San Jeronimo 1010 Colonia.San Jeronimo Lidice ',' 56.83.17.81','contacto@jardineria2000.com','https://jardineria2000.com/','Whatsapp 55 54 51 0083'),
+(168,'KOALA','KOALA','  Azcapotzalco, CDMX ',' 55.67.98.37.53','contacto@jgrupokoala.com.mx','https://www.grupokoala.com.mx/jardinera-koala','Whatsapp 55.84.24.4772'),
+(169,'ColibriJardineria','ColibriJardineria','  Xochimilco,Ciudad de Mexico ',' (55) 54898962  ','contacto@colibrijardineria.com.mx ','http://colibrijardineria.com.mx/','notiene'),
+(170,'ServiceMasterMexico','ServiceMasterMexico','  Aramberri 935 Poniente 2, Col. Centro, C.P. 64720, Monterrey, Nuevo León,  ','  01 52 (81)81342927 ','info@servicemastermexico.com ','https://www.servicemastermexico.com/jardineria.html','https://twitter.com/servicemasterm2'),
+(171,'Garden Central Jardineria ','Garden Central Jardineria ',' MIGUEL DE CERVANTES SAAVEDRA 47-D, COL. GRANADA, ALC. MIGUEL HIDALGO. CDMX 11520. ',' Tel1: 55.5282.3274  ','notiene ','https://garden-central.mx/','notiene'),
+(172,'Viverdi','Viverdi','  Xochimilco,Ciudad de Mexico ',' (81) 8387 0620   ','contacto@viverdimexico.com ','https://viverdimexico.com/jardines/','whatasapp+52 81 8273 2842 '),
+(173,'Jardineria Torres','Jardineria Torres',' Calzada Gómez morin #38
+21323 Mexicali
+México  ',' (81) 8387 0620    ','contacto@viverdimexico.com  ','https://www.homify.com.mx/profesionales/7152342/servicios-de-jardineria-torres','m.facebook.com/JardineriaTorres/?locale2=es_LA ')
 /******************332******************/
 
 /******************333******************/
