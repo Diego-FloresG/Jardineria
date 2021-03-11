bd4ca8ead2da55db801773257583f5bc0d28c2dc
Diego-FloresG <luis.flores16@tectijuana.edu.mx>
2021-03-10 21:22:23 -0800

    Actualizacion Diego

--- servicios.sql
+++ servicios.sql
@@ -1,5 +1,14 @@
 /******************1******************/
 insert into Servicios values
-
+(1, 'Diseño de jardinería'),
+(2, 'Limpieza de terrenos'),
+(3, 'Instalación de áreas verdes'),
+(4, 'Mantenimiento de áreas verdes'), 
+(5, 'Sistemas de riego'),
+(6, 'Fumigacion'),
+(7, 'Jardinería en general'),
+(8, 'Arada de nieve'),
+(9, 'Fertilización'),
+(10, 'Pasto')
 /************************************/
 
