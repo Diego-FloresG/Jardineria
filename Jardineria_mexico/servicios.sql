27f85ba2ace22a8e6c5d71b6d0328e28b73185a3
Diego-FloresG <luis.flores16@tectijuana.edu.mx>
2021-03-16 03:17:38 -0700

    Jardines_Mexico_Actulizacion_Total

---
+++ Jardineria_mexico/servicios.sql
@@ -0,0 +1,12 @@
+insert into Servicios values
+(1, 'Diseño de jardinería','Se desarrollan diseños de jardines para residencias y empresas'),
+(2, 'Limpieza de terrenos','Se realiza limpieza de terrenos ya sea de escombros, malezas y limpieza en general de áreas verdes'),
+(3, 'Instalación de áreas verdes','Se siembran y construyen los correspondientes elementos para mejorar los jardines de los clientes'),
+(4, 'Mantenimiento de áreas verdes','Se realiza servicio para mantener la forma de las áreas verdes, desde podar pasto, palmeras, arbustos, etc'), 
+(5, 'Sistemas de riego','Implementación de sistema de aspersores y mecanismos para mantener hidratado las áreas verdes para su correcto crecimiento'),
+(6, 'Fumigación','Se realiza los procedimiento necesarios para evitar o eliminar plagas que afecten las áreas verdes'),
+(7, 'Jardinería en general','Servicios de jardineria en general para residencias, riego de areas verdes, cortes de arboles, palmeras, pasto y limpieza en general'),
+(8, 'Arada de nieve','Se eliminar la nieve de inviernos de los jardines y su alrededor'),
+(9, 'Fertilización','Se realiza los procesos de fertilización de las plantas para asegurar el crecimiento a través de los productos escogidos por profesionales'),
+(10, 'Pasto','Servicios para poner pasto natural y artificial'),
+(11, 'Vivero','Instalación agronómica donde se cultivan, germinan y maduran todo tipo de plantas')
